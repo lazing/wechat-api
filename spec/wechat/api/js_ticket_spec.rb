dd330ed1736026d3424bfc4fc74b5fc7954da726
lazing <lazing@gmail.com>
2021-01-21 13:58:59 +0800

    fix api error

--- spec/wechat/api/js_ticket_spec.rb
+++ spec/wechat/api/js_ticket_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Wechat::Api::JsTicket do
   end
 
   it :refresh do
-    allow(client).to receive(:js_ticket).and_return(expires_in: 7200, ticket: 'bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA')
+    allow(client).to receive(:js_ticket).and_return('expires_in'=> 7200, 'ticket'=> 'bxLdikRXVbTPdHSM05e5u5sUoXNKd8-41ZO3MhKoyN5OfkWITDGgnr2fwJ0m9E8NYzWKVZvdVtaUgWvsdshFKA')
     subject.refresh
     expect(subject.ticket).not_to be_nil
     should_not be_expired
--- spec/wechat/api/util_spec.rb
+++ spec/wechat/api/util_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe Wechat::Api::Util do
   it :js_config do
     allow(SecureRandom).to receive(:hex).and_return('Wm3WZYTPz0wzccnW')
     allow_any_instance_of(Time).to receive(:to_i).and_return(1414587457)
-    allow(subject).to receive(:js_ticket).and_return(expires_in: 7200, ticket: 'sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg')
+    allow(subject).to receive(:js_ticket).and_return('expires_in' => 7200, 'ticket' => 'sM4AOVdWfPE4DxkXGEs8VMCPGGVi4C3VM0P37wVUCFvkVAy_90u5h9nbSlYy3-Sl-HhTdfl2fzFy1AOcHKP7qg')
     js = subject.js_config(url: 'http://mp.weixin.qq.com?params=value')
     expect(js).not_to be_nil
     expect(js).to include(signature: '0f9de62fce790f9a083d5c99e95740ceb90c27ed')
